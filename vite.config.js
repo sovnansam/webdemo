6e419e80d67590fa793437198fcc34af81c53a54
SAM <sam@adsl-172-10-0-205.dsl.sndg02.sbcglobal.net>
2025-12-11 16:19:34 +0700

    Update project

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,24 @@ export default defineConfig({
   server: {
     proxy: {
       '/API': {
-        target: 'http://203.189.137.34:1265/ksfh_backend',
+        target: 'http://203.189.137.34:1265/',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/API/, '')
+        rewrite: (path) => {
+          const newPath = path.replace(/^\/API/, '/ksfh_backend/API/');
+          console.log('Rewriting:', path, '→', newPath);
+          return newPath;
+        },
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log('Sending Request to the Target:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          });
+        }
       }
     }
   }
